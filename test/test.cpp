8d3743fe09f478a732c2047c51930ef19ad97b61
Korgopolov2006 <ztrovez@gmail.com>
2024-06-04 23:19:07 +0300

    first commit

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "..\libraryC++\Header.h"
+#include "Windows.h"
+
+int main()
+{
+    std::cout << Sum(35, 2);
+    HINSTANCE load = LoadLibrary(L"DinamicLibrary.dll");
+    typedef int (*sum) (int, int);
+    sum Sum;
+    Sum = (sum)GetProcAddress(load, "Sum");
+    std::cout << Sum(2, 5);
+    FreeLibrary(load);
+}
